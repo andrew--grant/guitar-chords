8d23074d19faa6cec655f8a40e2563a844b81a24
Andrew Grant <agrant@gme.net.au>
2016-07-14 14:12:24 +1000

    tidyied up graphics classes

--- snap-demo/main.js
+++ snap-demo/main.js
@@ -1,60 +1,79 @@
 /* Guitar class */
-var Guitar = function(svg) {
+var Guitar = function (svg, opts) {
     var s = Snap(svg);
     this.svg = s;
+    this.opts = opts;
 }
 
-Guitar.prototype.draw = function() {
-    var fret = new Fret(this.svg);
+Guitar.prototype.draw = function () {
+    var fret = new Fret(this.svg, this.opts);
     fret.draw();
 }
 
+
 /* Fret class */
-var Fret = function(svg) {
+var Fret = function (svg, opts) {
     this.svg = svg;
-    this.fretWidth = 300;
-    this.fretHeight = 325;
+    this.opts = opts;
     this.fretx = 50;
-    this.frety = 45;
-    this.fretbg = 'brown';
-    this.stringbg = 'red';
-    this.fingersize = ((this.fretHeight - 30) / 12) - 4;
-    this.spacer = 55;
+    this.frety = 50;
+    this.spacer = this.fretHeight / 6;
+    this.fretbg = opts.fretColour || 'brown';
+    this.stringbg = this.opts.stringColour || 'black';
+    this.fingersize = opts.fingerSize || 15;
 }
 
-Fret.prototype.draw = function() {
-    var fret = this.svg.rect(this.fretx, this.frety + 30, this.fretWidth, this.fretHeight);
-    fret.attr({
-        fill: this.fretbg
-    });
+Fret.prototype.draw = function () {
+    var fretHeight = 0;
     for (i = 1; i <= 6; i++) {
-        var finger = new Finger(this.svg);
-        finger.draw(this.fretx, this.frety + (i * this.spacer), this.fingersize);
+
+        var frety = this.frety + (i * (this.fingersize * 2) - this.fingersize);
+
+        var finger = new Finger(this.svg, this.opts);
+        finger.draw(this.fretx + (this.fingersize * 3), frety, this.fingersize);
+
+        var guitarString = new GuitarString(this.svg, this.opts);
+        guitarString.draw(this.fretx, frety, this.fingersize * 6);
+
+        fretHeight += this.fingersize * 2;
     }
+
+    var fret = this.svg.rect(this.fretx, this.frety, this.fingersize * 6, fretHeight);
+    fret.attr({
+        fill: this.fretbg,
+        opacity: .2
+    });
 }
 
 /* GuitarString class */
-var GuitarString = function(svg) {
+var GuitarString = function (svg, opts) {
     this.svg = svg;
+    this.stringbg = opts.stringColour;
 }
 
-GuitarString.prototype.draw = function(fretx, frety, fingersize) {}
-
-var Finger = function(svg) {
-    this.svg = svg;
-    this.fingerbg = 'blue';
+GuitarString.prototype.draw = function (guitarStringx, guitarStringy, width) {
+    var guitarString = this.svg.rect(guitarStringx, guitarStringy, width, 3);
+    guitarString.attr({
+        fill: this.stringbg
+    });
 }
 
-Finger.prototype.draw = function(fretx, frety, fingersize) {
-    var finger = this.svg.circle(fretx, frety, fingersize);
+/* Finger class */
+var Finger = function (svg, opts) {
+    this.svg = svg;
+    this.fingerbg = opts.fingerColour || 'blue';
+}
+
+Finger.prototype.draw = function (fingerx, fingery, fingersize) {
+    var finger = this.svg.circle(fingerx, fingery, fingersize);
     finger.attr({
         fill: this.fingerbg
     });
 }
 
 /* Entry point */
-window.onload = function() {
-    var guitar = new Guitar("#svg");
-    // todo: add options - height, width, colours, styles etc
+window.onload = function () {
+    // finger size is a base ratio
+    var guitar = new Guitar("#svg", { stringColour: 'black', fingerColour: 'red' });
     guitar.draw();
 }
