6cf21ba7ea713f883eb1ca8d220503974963273e
andrew grant <andrewgrant@hotmail.com>
2016-08-04 17:25:06 +1000

    UI changes around chord family selection

--- app/js/guitar/guitar-model.js
+++ app/js/guitar/guitar-model.js
@@ -17,8 +17,8 @@ function guitarModel() {
              */
             {
                 name: 'c',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
-                difficulty: 2,
+                families: ['c', 'd', 'major', 'f#m', 'bm', 'c#m'],
+                difficulty: 2, // remove, its just  a case of 'family'
                 shape: [
                     [1, 1, 0], // fret, string, finger 0 1 0 2 3
                     [1, 2, 1],
@@ -29,7 +29,7 @@ function guitarModel() {
                 ]
             }, {
                 name: 'a',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
+                families: ['a', 'suspended', 'e', 'f#m', 'bm', 'c#m'],
                 difficulty: 2,
                 shape: [
                     [3, 1, -1], // fret, string, finger
@@ -41,7 +41,7 @@ function guitarModel() {
                 ]
             }, {
                 name: 'b',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
+                families: ['b', 'minor', 'e', 'f#m', 'bm', 'c#m'],
                 difficulty: 2,
                 shape: [
                     [18, 1, 0], // fret, string, finger
@@ -53,7 +53,7 @@ function guitarModel() {
                 ]
             }, {
                 name: 'f',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
+                families: ['a', 'd', 'minor', 'f#m', 'f', 'c#m'],
                 difficulty: 2,
                 shape: [
                     [11, 1, -1], // fret, string, finger
@@ -65,7 +65,19 @@ function guitarModel() {
                 ]
             }, {
                 name: 'c-min-5',
-                families: ['g', 'c', 'd', 'em', 'am', 'bm'],
+                families: ['g', 'c', 'd', 'em', 'am', 'f', 'easy'],
+                difficulty: 4,
+                shape: [
+                    [3, 1, 1],
+                    [2, 2, 2],
+                    [1, 3, -1],
+                    [1, 4, 0],
+                    [1, 5, 0],
+                    [3, 6, 3]
+                ]
+            }, {
+                name: 'g',
+                families: ['g', 'minor', 'd', 'em', 'am', 'bm', 'easy'],
                 difficulty: 4,
                 shape: [
                     [3, 1, 1],
@@ -89,7 +101,7 @@ function guitarModel() {
                 ]
             }, {
                 name: 'e',
-                families: ['e', 'a', 'b', 'cm', 'fm', 'gm'],
+                families: ['e', 'a', 'b', 'cm', 'fm', 'gm', 'easy'],
                 difficulty: 3,
                 shape: [
                     [1, 1, 1],
@@ -113,7 +125,7 @@ function guitarModel() {
                 ]
             }, {
                 name: 'a',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
+                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m', 'easy'],
                 difficulty: 2,
                 shape: [
                     [3, 1, -1], // fret, string, finger
@@ -185,91 +197,7 @@ function guitarModel() {
                 ]
             }, {
                 name: 'c',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
-                difficulty: 2,
-                shape: [
-                    [1, 1, 0], // fret, string, finger 0 1 0 2 3
-                    [1, 2, 1],
-                    [1, 3, 0],
-                    [2, 4, 2],
-                    [3, 5, 3],
-                    [1, 6, -1]
-                ]
-            }, {
-                name: 'a',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
-                difficulty: 2,
-                shape: [
-                    [3, 1, -1], // fret, string, finger
-                    [3, 2, 0],
-                    [3, 3, 1],
-                    [3, 4, 2],
-                    [3, 5, 3],
-                    [3, 6, 0]
-                ]
-            }, {
-                name: 'b',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
-                difficulty: 2,
-                shape: [
-                    [18, 1, 0], // fret, string, finger
-                    [19, 2, -1],
-                    [17, 3, 1],
-                    [18, 4, 2],
-                    [18, 5, 3],
-                    [17, 6, 0]
-                ]
-            }, {
-                name: 'f',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
-                difficulty: 2,
-                shape: [
-                    [11, 1, -1], // fret, string, finger
-                    [11, 2, 0],
-                    [11, 3, 1],
-                    [12, 4, 2],
-                    [12, 5, 3],
-                    [11, 6, 0]
-                ]
-            }, {
-                name: 'g',
-                families: ['g', 'c', 'd', 'em', 'am', 'bm'],
-                difficulty: 4,
-                shape: [
-                    [3, 1, 1],
-                    [2, 2, 2],
-                    [1, 3, -1],
-                    [1, 4, 0],
-                    [1, 5, 0],
-                    [3, 6, 3]
-                ]
-            }, {
-                name: 'g6', // uses a bar ( -2)
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
-                difficulty: 2,
-                shape: [
-                    [7, 1, -2], // fret, string, finger
-                    [8, 2, 1],
-                    [7, 3, 0],
-                    [9, 4, 2],
-                    [7, 5, 0],
-                    [7, 6, 0]
-                ]
-            }, {
-                name: 'e',
-                families: ['e', 'a', 'b', 'cm', 'fm', 'gm'],
-                difficulty: 3,
-                shape: [
-                    [1, 1, 1],
-                    [2, 2, 1],
-                    [3, 3, 1],
-                    [4, 4, 1],
-                    [5, 5, 1],
-                    [6, 6, 1]
-                ]
-            }, {
-                name: 'c',
-                families: ['a', 'd', 'e', 'f#m', 'bm', 'c#m'],
+                families: ['c', 'f', 'e', 'f#m', 'bm', 'c#m'],
                 difficulty: 2,
                 shape: [
                     [1, 1, 0], // fret, string, finger 0 1 0 2 3
